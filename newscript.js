3018d923c15de07f5d37ccb499b495ce502216c6
Ivantay12 <168537315+Ivantay12@users.noreply.github.com>
2024-05-23 17:03:58 +0000

    24 May

--- newscript.js
+++ newscript.js
@@ -1,7 +1,27 @@
 document.getElementById('generateButton').addEventListener('click', function() {
+    const drinks = [
+        "Antelope Cooling Water 羚羊清热水",
+        "Water Chestnut Drink 马蹄水",
+        "Honey Aloe Vera Drink 芦丁蜂蜜汁",
+        "Barley Drink 薏米水",
+        "Luo Han Guo 罗汉果茶",
+        "Honey Wheat Grass 麦草蜂蜜水",
+        "Roselle Hawthorn Tea 洛神花山楂茶",
+        "Winter Melon Tea with Honey 蜂蜜冬瓜茶",
+        "Almond Figs Tea 川贝杏仁茶",
+        "Honeysuckle Flower Tea 金银花凉茶",
+        "Ge Gen Tea 葛根茶",
+        "Prunella Tea 夏古草",
+        "Peppermint Tea 薄荷茶",
+        "Shi Huo Tea 石霍凉茶",
+        "Raspberry Leaf Tea 覆盆子茶",
+        "Lemon Balm Tea 香蜂草茶",
+        "Chamomile Tea 甘菊茶"
+    ];
+    
+    const randomDrink = drinks[Math.floor(Math.random() * drinks.length)];
+    
     const content = document.getElementById('generatedContent');
-    content.innerHTML = `
-        <p>"Angmoh Liang Teh" Beer</p>
-    `;
+    content.innerHTML = `<b>${randomDrink}</b>`;
     content.style.display = 'block';
 });
